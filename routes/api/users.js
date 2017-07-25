c9363a44380e154750c375a4f133ffe31121c0d5
LiubomyrPenkov <lubchikpenkov@gmail.com>
2017-07-25 14:23:42 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,10 +6,9 @@ const Joi = require('joi');
 const User = require('../../models/user');
 const Group = require('../../models/group');
 
-
 router.route('/users')
     //get users
-    .get((req, res, next) => {
+    .get(async (req, res, next) => {
         //check if request query was sent
         if (Object.keys(req.query).length) {
             //validation request query data
@@ -21,22 +20,21 @@ router.route('/users')
                 next(result.error);
             }
             //find all users where "role":req.query.role
-            User.find({ "role": req.query.role })
-                .exec()
-                .then((users) => {
-                    res.status(200).json(users);
-                })
-                .catch((err) => next(err));
+            try {
+                res.status(200).json(await User.find({ "role": req.query.role }).exec());
+            } catch (err) {
+                next(err);
+            }
         } else {
-            User.find({}).exec()
-                .then((users) => {
-                    res.status(200).json(users);
-                })
-                .catch((err) => next(err));
+            try {
+                res.status(200).json(await User.find({}).exec());
+            } catch (err) {
+                next(err);
+            }
         }
     })
     //post user
-    .post((req, res, next) => {
+    .post(async (req, res, next) => {
         //validation req.body
         const schema = Joi.object().keys({
             username: Joi.string().alphanum().min(4).max(20).required(),
@@ -48,64 +46,61 @@ router.route('/users')
         if (result.error) {
             next(result.error);
         }
-        //check if username already exists
-        User.count({ "username": req.body.username })
-            .exec()
-            .then((users) => {
-                if (users) {
-                    throw new Error("User already exists, please choose another user's name");
-                }
-            })
-            .then(() => {
-                if (req.body.role === "superadmin") {
-                    return User.count({ "role": "superadmin" })
-                }
-            })
-            .then((count) => {
-                if (count == 2) {
-                    throw new Error("can't be more than two superadmins")
-                }
-            })
-            //if username doesn't exist - create
-            .then(() => User.create(req.body))
-            .then((user) => res.status(201).json(user))
-            .catch((err) => next(err));
+        try {
+            //check if username already exists
+            const userCount = await User.count({ "username": req.body.username }).exec();
+            if (userCount) {
+                throw new Error("User already exists, please choose another user's name");
+            }
+            if (req.body.role === "superadmin") {
+                if (req.user[0].role !== 'superadmin') { throw new Error("Only superadmin can create another superadmin") };
+                //check how many superadmins there are
+                const superCount = await User.count({ "role": "superadmin" }).exec();
+                if (superCount == 2) { throw new Error("can't be more than two superadmins") }
+            }
+            res.status(201).json(await User.create(req.body));
+        } catch (err) {
+            next(err)
+        }
     })
 
 router.route('/users/:_id')
     //get user by id
-    .get((req, res, next) => {
-        User.find({ "_id": req.params._id })
-            .exec()
-            .then((user) => { res.status(200).json(user) })
-            .catch((err) => { next(err) })
+    .get(async (req, res, next) => {
+        try {
+            res.status(200).json(await User.find({ "_id": req.params._id }).exec())
+        } catch (err) {
+            next(err);
+        }
     })
     //delete user by id
-    .delete((req, res, next) => {
-        //check if this user is superadmin
-        User.find({ "_id": req.params._id, "role": "superadmin" })
-            .exec()
-            //check if it's the last superadmin
-            .then((user) => {
-                if (user.length) {
-                    return User.count({ "role": "superadmin" });
-                }
-            })
-            //if it's the last superadmin - error - cancel deletion
-            .then((count) => {
-                if (count === 1) {
-                    throw new Error("It must be at least one superadmin");
-                }
-            })
+    .delete(async (req, res, next) => {
+        try {
+            //check if autorized user want to delete oneself(if yes -> allow deletion)->if not - check if 
+            //autorzed user is admin or superadmin -> if not - throw Error
+            if (req.user[0].id !== req.params._id && req.user[0].role === 'user') {
+                throw new Error("Only admin or superadmin can delete another user")
+            }
+            //check if this user is superadmin
+            const user = await User.find({ "_id": req.params._id, "role": "superadmin" }).exec();
+            if (user.length) {
+                //check if autorized user is superadmin
+                if (req.user[0].role !== 'superadmin') { throw new Error("Only superadmin can delete another superadmin") };
+                const superCount = await User.count({ "role": "superadmin" }).exec();
+                //check if it's the last superadmin
+                if (superCount === 1) { throw new Error("It must be at least one superadmin"); }
+            }
             //delete user
-            .then(() => User.remove({ "_id": req.params._id }))
+            await User.remove({ "_id": req.params._id }).exec();
             //delete user's id from groups
-            .then(() => Group.update({}, { $pull: { "participants": { $in: req.params._id } } }, { multi: true }))
-            .then(() => { res.status(200).json({ "message": "the user is deleted" }) })
-            .catch((err) => { next(err) })
+            await Group.update({}, { $pull: { "participants": { $in: req.params._id } } }, { multi: true }).exec();
+            res.status(200).json({ "message": "the user is deleted" });
+        } catch (err) {
+            next(err);
+        }
     })
     //update user
-    .patch((req, res, next) => {
+    .patch(async (req, res, next) => {
         //req.body validation
         const schema = Joi.object().keys({
             username: Joi.string().alphanum().min(4).max(20),
@@ -117,37 +112,41 @@ router.route('/users/:_id')
         if (result.error) {
             next(result.error);
         }
-        //check the "role" in request body + how many superadmins there are
-        if (req.body.role === "superadmin") {
-            User.count({ "role": "superadmin" })
-                .exec()
-                .then((users) => {
-                    if (users === 2) {
-                        throw new Error("can't be more than two superadmins");
-                    }
-                })
-                .then(() => User.update({ "_id": req.params._id }, { $set: req.body }, { new: true }))
-                .then((user) => { res.status(200).json({ "message": "user is updated" }) })
-                .catch((err) => next(err));
-        } else {
-            //check if superadmin is wanted to be changed
-            User.count({ "_id": req.params._id, "role": "superadmin" })
-                .exec()
-                .then((count) => {
-                    if (count) {
-                        //if it is superadmin - check how many superadmins there are
-                        return User.count({ "role": "superadmin" })
-                    }
-                })
-                .then((count) => {
+        try {
+            //check if autorized user want to change oneself(if yes -> allow changes)->if not - check if 
+            //              autorized user is admin or superadmin -> if not - throw Error
+            if (req.user[0].id !== req.params._id && req.user[0].role === 'user') {
+                throw new Error("Only admin or superadmin can change another user")
+            }
+            //check the "role" in request body + how many superadmins there are
+            if (req.body.role === "superadmin") {
+                //check authorized user's role
+                if (req.user[0].role !== 'superadmin') { throw new Error("Only superadmin can create another superadmin") }
+                const usersCount = await User.count({ "role": "superadmin" }).exec();
+                const isSuper = await User.count({ _id: req.params._id, "role": "superadmin" }).exec();
+                if (usersCount === 2 && !isSuper) {
+                    throw new Error("can't be more than two superadmins");
+                }
+                await User.update({ "_id": req.params._id }, { $set: req.body }, { new: true }).exec();
+                res.status(200).json({ "message": "user is updated" })
+            } else {
+                //check if superadmin is wanted to be changed
+                const count = await User.count({ "_id": req.params._id, "role": "superadmin" }).exec();
+                if (count) {
+                    //if it is superadmin -> check authorized user's role
+                    if (req.user[0].role !== 'superadmin') { throw new Error("Only superadmin can change another superadmin") };
+                    //check how many superadmins there are
+                    const count = await User.count({ "role": "superadmin" }).exec();
                     //if it's the last superadmin - cancel updation
                     if (count === 1) {
                         throw new Error("Can't change this user, because it must be at least one superadmin ")
                     }
-                })
-                .then(() => User.update({ "_id": req.params._id }, { $set: req.body }, { new: true }))
-                .then((user) => { res.status(200).json({ "message": "user is updated" }) })
-                .catch((err) => { next(err) })
+                }
+                await User.update({ "_id": req.params._id }, { $set: req.body }, { new: true }).exec();
+                res.status(200).json({ "message": "user is updated" });
+            }
+        } catch (err) {
+            next(err);
         }
     });
 module.exports = router;
